e287eac72ff255db0ccaf69102dbdb6a7eb69ba5
Ben Chavez <ben.m.chavez@gmail.com>
2020-08-20 14:26:29 -0500

    take in search field and conosole log input

--- Section 2: React Basics/02-React-Basics - Exercises/03-My-first-app/src/components/search_bar.jsx
+++ Section 2: React Basics/02-React-Basics - Exercises/03-My-first-app/src/components/search_bar.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  handleUpdate(event) => {
-    // START HERE BY CONSOLE LOGGING THE INPUT OF THE SEARCH BAR
-  }
-  
+  handleUpdate = (event) => {
+    // will need to take this input and us it to search
+    console.log(event.target.value);
+  };
+
   render() {
     return (
       <input
         type='text'
         className='form-search form-control'
-        onChange='this.handleUpdate'
+        onChange={this.handleUpdate}
       />
     );
   }
